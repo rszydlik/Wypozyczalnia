646c3a8e8ffab50d7f37ad7f58993e1f5d1359fe
rszydlik <r.szydlik@wp.pl>
2020-02-05 16:25:35 +0100

    wprowadzanie friendsow i korekta biblioteczek

--- wypozyczalnia_app/admin.py
+++ wypozyczalnia_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from wypozyczalnia_app.models import Book
+from wypozyczalnia_app.models import Book, Library
 
 # Register your models here.
 
 admin.site.register(Book)
+admin.site.register(Library)
--- wypozyczalnia_app/models.py
+++ wypozyczalnia_app/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     btitle = models.CharField(max_length=20)
     bauthor = models.CharField(max_length=20)
     bdescription = models.TextField(max_length=100)
-    owners = models.ManyToManyField(User)
+    owners = models.ManyToManyField(User, through='Library')
 
     def __str__(self):
         return self.btitle
@@ -15,3 +15,17 @@ class Book(models.Model):
     class Meta:
         db_table = "books"
 
+
+class Library(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Friend(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    relates = models.ManyToManyField(User, through='Network')
+
+
+class Network(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
